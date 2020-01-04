fdc29149dc22f3c84fc22120340bdc0745ac6a86
galesky <leonardo.galesky@gmail.com>
2020-01-03 22:25:40 -0300

    fix(): update override with generics

--- src/overrideWithGeneric.decorator.ts
+++ src/overrideWithGeneric.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, PipeTransform } from '@nestjs/common';
 
-export const OverrideWithGeneric = createParamDecorator((req , data) => {
+export const OverrideWithGeneric = createParamDecorator<Request, string>((req , data) => {
   return req.body;
 });
